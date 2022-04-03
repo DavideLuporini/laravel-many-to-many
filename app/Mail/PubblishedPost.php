ed16a6306c46ec77ef172c11d2aaa4430ba3d235
illupovero <davideluporini1996@gmail.com>
2022-04-04 00:58:07 +0200

    error in create

--- app/Mail/PubblishedPost.php
+++ app/Mail/PubblishedPost.php
@@ -11,14 +11,15 @@ class PubblishedPost extends Mailable
 {
     use Queueable, SerializesModels;
 
+    private $post;
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($post)
     {
-        //
+        $this->post = $post;
     }
 
     /**
@@ -28,6 +29,6 @@ class PubblishedPost extends Mailable
      */
     public function build()
     {
-        return $this->view('mails.posts.pubblished');
+        return $this->view('mails.posts.pubblished', ['post' => $this->post]);
     }
 }
--- resources/views/mails/posts/pubblished.blade.php
+++ resources/views/mails/posts/pubblished.blade.php
@@ -1,1 +1,36 @@
-post inviato
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<style>
+    body{
+        background-color: whitesmoke;
+    }
+
+    ul{
+        list-style-type: none;
+    }
+</style>
+<body>
+    
+    <h1>CONGRATULATION! The post :{{$post->title}} èhas been pubblished correctly!</h1>
+    <h2>date : {{$post->getFormattedDate('created_at')}}</h2>
+
+    <h3>Category : {{$post->category->label}}</h3>
+
+    <ul>
+        @forelse($post->tags as $tag)
+        <li>
+            {{$tag->label}}
+        </li>
+        @empty
+        <li>No tags</li>
+        @endforelse
+    </ul>
+
+</body>
+</html>
